c6445ae35b93f609773f4a3e9dc1f1d3f5a82314
yannou38 <yann.ducruy@live.fr>
2016-12-15 22:04:18 +0100

    reindented stuff and changed filename

--- warnbot.js
+++ warnbot.js
@@ -8,68 +8,68 @@ var sql = require('./sql.json')
 
 //This is the MySQL Connection stuff, configurated on sql.json
 var connection = mysql.createConnection({
-  host: sql.host,
-  user: sql.user,
-  password: sql.password,
-  database: sql.database
+	host: sql.host,
+	user: sql.user,
+	password: sql.password,
+	database: sql.database
 });
 
 connection.connect();
 
 //Commands
 const commands = {
-'warn': (message) => {
-  const mentionedUser = message.mentions.users.first();
-  let  args = message.content.split(mentionedUser).slice(1);
-  message.reply("Warned " + message.mentions.users.first() + ".\nHis ID is: " + message.mentions.users.first().id + " you can use this on the bot's website to check if the user has any warns")
-  mentionedUser.sendMessage("You got warned on server " + message.guild.name + " by the user with KICK_MEMBERS permissions named " + message.author.username + " with reason\n" + args.join(" "))
-  console.log('[C] ' + message.author.username + ' Warned ' + message.mentions.users.first() + ' On ' + message.guild.name)
-  var warn = {
-    "user": "" + message.author.username + "",
-    "discord_id": "" + message.mentions.users.first().id + "",
-    "reason": "" + args.join(" ") + "",
-  }
+	'warn': (message) => {
+		const mentionedUser = message.mentions.users.first();
+		let  args = message.content.split(mentionedUser).slice(1);
+		message.reply("Warned " + message.mentions.users.first() + ".\nHis ID is: " + message.mentions.users.first().id + " you can use this on the bot's website to check if the user has any warns")
+		mentionedUser.sendMessage("You got warned on server " + message.guild.name + " by the user with KICK_MEMBERS permissions named " + message.author.username + " with reason\n" + args.join(" "))
+		console.log('[C] ' + message.author.username + ' Warned ' + message.mentions.users.first() + ' On ' + message.guild.name)
+		var warn = {
+			"user": "" + message.author.username + "",
+			"discord_id": "" + message.mentions.users.first().id + "",
+			"reason": "" + args.join(" ") + "",
+		}
 
-  connection.query("INSERT INTO warns SET ?", warn, function(error) {
-    if (error) {
-      console.log(error);
-      return;
-    }
-  })
-},
-'eval': (message) => {
-  if(!message.author.id === config.admin_id) return;
-  let  args = message.content.split(" ").slice(1);
-  function clean(text) {
-  if (typeof(text) === "string")
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else
-      return text;
-}
-const params = message.content.split(" ").slice(1);
-const command = args.shift().slice(config.prefix.length);
+		connection.query("INSERT INTO warns SET ?", warn, function(error) {
+			if (error) {
+				console.log(error);
+				return;
+			}
+		})
+	},
+	'eval': (message) => {
+		if(!message.author.id === config.admin_id) return;
+		let  args = message.content.split(" ").slice(1);
+		function clean(text) {
+			if (typeof(text) === "string")
+				return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+			else
+				return text;
+		}
+		const params = message.content.split(" ").slice(1);
+		const command = args.shift().slice(config.prefix.length);
 
-  try {
-    var code = params.join(" ");
-    var evaled = eval(code);
+		try {
+			var code = params.join(" ");
+			var evaled = eval(code);
 
-    if (typeof evaled !== "string")
-      evaled = require("util").inspect(evaled);
+			if (typeof evaled !== "string")
+				evaled = require("util").inspect(evaled);
 
-    message.channel.sendCode("xl", clean(evaled));
-  } catch(err) {
-    message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-  }
-}
+			message.channel.sendCode("xl", clean(evaled));
+		} catch(err) {
+			message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+		}
+	}
 };
 
 bot.on("message", message => {
-  if (!message.content.startsWith(config.prefix)) return;
-  if (commands.hasOwnProperty(message.content.toLowerCase().slice(config.prefix.length).split(' ')[0])) commands[message.content.toLowerCase().slice(config.prefix.length).split(' ')[0]](message);
+	if (!message.content.startsWith(config.prefix)) return;
+	if (commands.hasOwnProperty(message.content.toLowerCase().slice(config.prefix.length).split(' ')[0])) commands[message.content.toLowerCase().slice(config.prefix.length).split(' ')[0]](message);
 });
 
 bot.on('ready', () => {
-  console.log(`This means the bot is online \n Hello world :)`);
+	console.log(`This means the bot is online \n Hello world :)`);
 });
 
 
